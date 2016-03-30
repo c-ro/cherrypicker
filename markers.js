84af59ce3495ead450b10fb18cdc42659b5db11b
c-ro <carlryanphoto@gmail.com>
2016-03-30 11:26:49 -0400

    added a few functions to markes module, started testing some cherrypicker-specific functions

--- markers.js
+++ markers.js
@@ -1,22 +1,30 @@
+"use stict";
+
 function markers() {
-	var markdown = {};
+	// define the object that will be exported as the markers module
+	var markers = {};
 	
-	horizontalRule = function(){
+	// define some useful functions for the module
+	markers.hr = function(){
 		var hr = "\n_____\n\n";
 		return hr;
 	};
+
+	markers.br = function(){
+		return "\n\n";
+	};
 	
-	makeLink = function(string, url){
+	markers.makeLink = function(string, url){
 		var link = "[" + string + "](" + url + ")";
 		return link;
 	};
 	
-	makeBold = function(string){
+	markers.bold = function(string){
 		var boldString = "**" +  string + "**";
 		return boldString;
 	};
 	
-	makeList = function(array, type = "ul"){
+	markers.makeList = function(array, type = "ul"){
 		var list = '';
 		if(type === "ol"){
 			array.forEach(function(element, index, array){
@@ -26,25 +34,40 @@ function markers() {
 			list = "* " + array.join('\n* ');
 		}
 		
-		return list
-	}
-	
-	return {
-		makeLink: makeLink,
-		makeList: makeList,
-		horizontalRule: horizontalRule,
-		makeBold: makeBold
+		return list;
 	};
+	
+	markers.section = function(content){
+		return this.br() + content + this.hr();
+	};
+
+	// return the markers object you can use those functions elsewhere
+	return markers;
 }
 
 var mrk = markers();
+///// module defined and instantiated, have fun!
 
-///// module defined and instantiated
 
-for (var i = 0; i < 3; i++){
-	console.log(mrk.makeBold("Homecity BeisbolCats"));
-	console.log(mrk.makeLink("Stream", "http://www.stream.com"));
-	console.log(mrk.horizontalRule());
-	console.log(mrk.makeList(["one", "two", "three"], "ol"));
-	console.log(mrk.makeList(["one", "two", "three"]));
+///  testing a cherrypicker-specific template helpers:
+function makeHeader(){
+    var string = "home" + " vs. " + "away";
+    return mrk.bold(string);
 }
+
+function makeScore(){
+ return mrk.hr() + mrk.bold("CURRENT SCORE: ") + "3" + " - " + "1" + mrk.br() + "Last Updated: " + "55:55:55";
+}
+
+/// markers
+console.log(mrk.section(makeHeader()));
+console.log(mrk.bold("Homecity BeisbolCats"));
+console.log(mrk.makeLink("Stream", "http://www.stream.com"));
+console.log(mrk.hr());
+console.log(mrk.makeList(["one", "two", "three"], "ol"));
+console.log(mrk.br());
+console.log(mrk.makeList(["one", "two", "three"]));
+
+// cherrypicker
+console.log(makeHeader());
+console.log(makeScore());
